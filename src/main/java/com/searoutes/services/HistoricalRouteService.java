65b9a39db6b7d2fa4526c292d788c823405ad59f
Daniel Bitka <daniel.bitka@gmail.com>
2023-04-22 16:00:12 +0200

    test ost

--- src/main/java/com/searoutes/services/HistoricalRouteService.java
+++ src/main/java/com/searoutes/services/HistoricalRouteService.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class HistoricalRouteService {
-
+//TODO 12
     private final SearchTypeService searchTypeService;
 
     public HistoricalRoute findHistoricalRoute(String typeSearch) {
-
         return searchTypeService.getRouteByTypeSearch(typeSearch);
     }
 
